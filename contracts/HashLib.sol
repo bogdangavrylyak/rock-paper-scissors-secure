42cc6b4990c140677a102f6c7b9f05e330dc350d
bogdangavrylyak <gavrylyakbogdan@gmail.com>
2023-06-12 23:11:10 +0300

    fixed logic errors

--- contracts/HashLib.sol
+++ contracts/HashLib.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "./RockPaperScissors.sol";
+import './RockPaperScissors.sol';
 
 library HashLib {
-    function hashChoice(RockPaperScissors.Choice _choice, bytes32 _salt) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_choice, _salt));
-    }
+  function hashChoice(
+    RockPaperScissors.Choice _choice,
+    bytes32 _salt
+  ) internal pure returns (bytes32) {
+    return keccak256(abi.encodePacked(_choice, _salt));
+  }
 }
